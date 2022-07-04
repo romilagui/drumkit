55a539d718d7e7762b9689b3be56ebf87b890347
Romila Rangel <85246670+romilagui@users.noreply.github.com>
2022-07-04 10:02:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -49,5 +49,11 @@ function makeSound(key) {
 }
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+   }, 100);
 }
 }
+
+
